1eafc1eaff54c757ea0f1ae85d239b1378afacfe
Pradyumnay14380 <pradyumnay14@gmail.com>
2025-04-29 21:11:06 +0530

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,23 @@
+
+import streamlit as st
+import joblib
+import numpy as np
+
+# Load model
+model = joblib.load('logistic_model.joblib')
+
+st.title("🌸 Iris Setosa Prediction App (Joblib Version)")
+
+sepal_length = st.slider('Sepal Length (cm)', 4.0, 8.0, 5.0)
+sepal_width = st.slider('Sepal Width (cm)', 2.0, 5.0, 3.0)
+petal_length = st.slider('Petal Length (cm)', 1.0, 7.0, 4.0)
+petal_width = st.slider('Petal Width (cm)', 0.1, 3.0, 1.0)
+
+input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+
+if st.button("Predict"):
+    prediction = model.predict(input_data)
+    if prediction[0] == 1:
+        st.success("This is likely an Iris Setosa 🌸")
+    else:
+        st.warning("This is likely NOT an Iris Setosa.")
